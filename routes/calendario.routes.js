d09ee418c010c3e6da9f3b512e9a560166e3bbb2
JuanBeta12 <juaebetancur1@misena.edu.co>
2024-09-11 00:31:05 -0500

    calendario juanes

---
+++ routes/calendario.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const eventos = require('../controller/calendarioController');
+const router = express.Router();
+
+router.get('/traer_eventos', eventos.traerEventos);
+router.get('/obtener_evento/:id', eventos.obtenerEvento);
+router.post('/agregar_evento', eventos.agregarEvento);
+router.post('/actualizar_evento/:id', eventos.actualizarEvento);
+router.delete('/eliminar_evento/:id', eventos.eliminarEvento);
+
+router.get('/modalUpdateEvento', function(req, res) {
+    res.render('modalUpdateEvento.ejs'); 
+});
+
+module.exports = router;
