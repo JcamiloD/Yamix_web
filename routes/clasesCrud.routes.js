46661a25f391b2fe956b7927576f6ef014aeda86
JuanBeta12 <juaebetancur1@misena.edu.co>
2024-09-25 03:10:29 -0500

    clases funcional

--- routes/clasesCrud.routes.js
+++ routes/clasesCrud.routes.js
@@ -3,26 +3,17 @@ const router = express.Router();
 const { restrictToPermiso } = require('../controller/middleware/rediect');
 const { attachUserPermissions } = require('../controller/middleware/permisosParaVistas');
 const { verifyToken } = require('../controller/middleware/verificarToken');
-const clases = require('../controller/clases_crud');
+const clases = require('../controller/clases_crud'); // Aquí usas 'clases'
 
-// Controlador artes mixtas
-router.get('/mixtas', verifyToken, restrictToPermiso('mixtas'),  clases.traer, (req, res) => {
-    res.render('./dashboard/mixtas', { data: res.locals.data });
-});
+// Rutas para el CRUD de clases
+router.get('/traer_clases', clases.traerClases);  // Traer todas las clases
+router.post('/agregar_clase', clases.agregarClase);  // Agregar una nueva clase
+router.post('/actualizar_clase/:id', clases.actualizarClase);  // Actualizar una clase existente
+router.delete('/eliminar_clase/:id', clases.eliminarClase);  // Eliminar una clase por ID
+router.get('/obtener_clase/:id_clase', clases.obtenerClase);
 
-// Controlador boxeo
-router.get('/boxeo', verifyToken,restrictToPermiso('boxeo'),  clases.traer, (req, res) => {
-    res.render('./dashboard/boxeo', { data: res.locals.data });
-});
-// Controlador parkour
-router.get('/parkour', verifyToken,restrictToPermiso('parkour'),  clases.traer, (req, res) => {
-    res.render('./dashboard/parkour', { data: res.locals.data });
-});
-
-// Controlador clases
-router.get('/clases', verifyToken, restrictToPermiso('clases'),  clases.traer, (req, res) => {
+router.get('/clases', verifyToken, restrictToPermiso('clases'), clases.traerClases, (req, res) => {
     res.render('./dashboard/clases', { data: res.locals.data });
 });
-
 
 module.exports = router;
